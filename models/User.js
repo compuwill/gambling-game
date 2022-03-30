a83824f0bdd9801a40ec9a59017c4f4c009ad38c
compuwill <wpvorngsam@hotmail.com>
2022-03-29 20:20:37 -0500

    ADDED COMMENTS TO THE User.js file

--- models/User.js
+++ models/User.js
@@ -8,7 +8,9 @@ class User extends Model {
   }
 }
 
+// define table columns and configuration
 User.init(
+  // ⬇ TABLE COLUMN DEFINITIONS GO HERE ⬇
   {
     id: {
       type: DataTypes.INTEGER,
@@ -43,10 +45,15 @@ User.init(
         return newUserData;
       },
     },
+    // pass in our imported sequelize connection (the direct connection to our database)
     sequelize,
+    // don't automatically create createdAt/updatedAt timestamp fields
     timestamps: false,
+    // don't pluralize name of database table
     freezeTableName: true,
+    // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
     underscored: true,
+    // make it so our model name stays lowercase in the database
     modelName: 'user',
   }
 );
