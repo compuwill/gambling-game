a36c9dfb3b818b212a897b0b65472e0d81d830ca
compuwill <wpvorngsam@hotmail.com>
2022-04-04 20:50:51 -0500

    Fixed history records

--- controllers/api/game-routes.js
+++ controllers/api/game-routes.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { User } = require("../../models");
+const { User, History } = require("../../models");
 const withAuth = require('../../utils/auth');
 
 const checkResult = require("../../utils/game-logic");
